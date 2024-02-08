edb417a1f0956ddfef1d1216d0e0f9abcca5428d
ericplaquevent <eric.plaquevent@gmail.com>
2024-02-08 14:08:49 +0100

    update resume with EIB experience

--- js/script.js
+++ js/script.js
@@ -1,8 +1,5 @@
 (function() {
-  var getAgeFromBirthDate = function getAgeFromBirthDate(birthdate) {
-    return Math.floor(((new Date() - new Date(birthdate)) / 1000 / (60 * 60 * 24)) / 365.25);
-  };
-
-  var age = getAgeFromBirthDate('1988-02-02');
+  const getAgeFromBirthDate = (birthdate) => Math.floor(((new Date() - new Date(birthdate)) / 1000 / (60 * 60 * 24)) / 365.25);
+  const age = getAgeFromBirthDate('1988-02-02');
   document.getElementById("age").innerHTML = age;
 })();
